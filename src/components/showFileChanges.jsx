98c8b597e84ae26fd178de60e8ab571966ac2a92
sban <soumya.banerjee@mountblue.io>
2019-10-14 01:08:24 +0530

    Used diff2html module to better highlight changes in files for a particular commit

--- src/components/showFileChanges.jsx
+++ src/components/showFileChanges.jsx
@@ -1,13 +1,28 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import {Diff2Html} from 'diff2html';
 
 class showFileChanges extends Component {
-  render() {
+  diffToHtml = (changedFiles) => {
+    setTimeout(function(){
+        // Everything will have rendered here
+        // let gitShowText = changedFiles;
+        // let diffStartIndex = gitShowText.indexOf('diff');
+        // let diffText = gitShowText.slice(diffStartIndex, gitShowText.length);
+        let diffText = changedFiles;
+        let outputHtml = Diff2Html.getPrettyHtml(diffText, {inputFormat: 'diff', showFiles: true, matching: 'lines'});
+        this.outputHtml = outputHtml;
+        document.querySelector(".diff-to-html").innerHTML = this.outputHtml;
+      }, 1000);
+  }
+
+    render() {
     return (
       <React.Fragment>
         <div>
                 <h4>File Changes</h4>
-                <p>{this.props.changedFiles}</p>
+                <p className='diff-to-html'>{this.props.changedFiles}</p>
+                {this.diffToHtml(this.props.changedFiles[0])}
                 </div>
       </React.Fragment>
     );
