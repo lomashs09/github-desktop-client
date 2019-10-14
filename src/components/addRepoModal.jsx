ca17b1286d353d60e904515b62b86be561bb0c4a
amoghwagh <amoghwagh@gmail.com>
2019-10-14 10:56:45 +0530

    Merged with Develop

--- src/components/addRepoModal.jsx
+++ src/components/addRepoModal.jsx
@@ -6,12 +6,12 @@ import { ipcRenderer, shell } from 'electron';
 
 export default class AddRepoModal extends Component {
   state = {
-    path: 'click on choose to add path...',
+    path: 'click on choose to add path...'
   };
-onChange = (e) =>{
-  this.props.repoToClonePath(e.target.value)
-  // this.props.repoToCloneUrl.setState({repoToCloneUrl:e.target.value})
-}
+  onChange = e => {
+    this.props.repoToClonePath(e.target.value);
+    // this.props.repoToCloneUrl.setState({repoToCloneUrl:e.target.value})
+  };
   componentDidMount() {
     ipcRenderer.on('folderPath', (event, arg) => {
       this.setState({
@@ -22,89 +22,94 @@ onChange = (e) =>{
   }
 
   render() {
-    if(this.props.selectedModal == 'new-repo'){
-    return (
-      <div id="modal1" className={`modal + ${this.props.modalDisplayClass}`}>
-        <div className="modal-content white">
-          <h4>ADD LOCAL REPOSITORY</h4>
-          <h6>Local Path</h6>
-          <ul className="collection">
-            <li className="collection-item path-location">{this.state.path}</li>
-          </ul>
-          <a
-            className="waves-effect waves-light btn-small grey lighten-2 black-text align-right"
-            onClick={() => {
-              ipcRenderer.send('add local repo', 'ADD_REPO');
-            }}
-          >
-            Choose...
-          </a>
-        </div>
-        <div className="modal-footer">
-          <a
-            className="modal-close waves-effect waves-green btn-flat"
-            onClick={() => {
-              this.props.toggleOverlay();
-              this.props.toggleModalClass();
-            }}
-          >
-            CLOSE
-          </a>
-          <a
-            className="add-repo waves-effect waves-green btn-flat blue darken-2 white-text"
-            onClick={() => {
-              this.props.toggleOverlay();
-              this.props.toggleModalClass();
-              this.props.setRepoDetailsDisplayClass(this.state.path[0]);
-              // console.log(this.state.path[0]);
+    if (this.props.selectedModal == 'new-repo') {
+      return (
+        <div id="modal1" className={`modal + ${this.props.modalDisplayClass}`}>
+          <div className="modal-content white">
+            <h4>ADD LOCAL REPOSITORY</h4>
+            <h6>Local Path</h6>
+            <ul className="collection">
+              <li className="collection-item path-location">{this.state.path}</li>
+            </ul>
+            <a
+              className="waves-effect waves-light btn-small grey lighten-2 black-text align-right"
+              onClick={() => {
+                ipcRenderer.send('add local repo', 'ADD_REPO');
+              }}
+            >
+              Choose...
+            </a>
+          </div>
+          <div className="modal-footer">
+            <a
+              className="modal-close waves-effect waves-green btn-flat"
+              onClick={() => {
+                this.props.toggleOverlay();
+                this.props.toggleModalClass();
+              }}
+            >
+              CLOSE
+            </a>
+            <a
+              className="add-repo waves-effect waves-green btn-flat blue darken-2 white-text"
+              onClick={() => {
+                this.props.toggleOverlay();
+                this.props.toggleModalClass();
+                this.props.setRepoDetailsDisplayClass(this.state.path[0]);
+                // console.log(this.state.path[0]);
 
-              // shell.openItem(this.state.path[0]);
-               // Opens the chosen folder/file in a new window
-            }}
-          >
-            ADD REPOSITORY
-          </a>
+                // shell.openItem(this.state.path[0]);
+                // Opens the chosen folder/file in a new window
+              }}
+            >
+              ADD REPOSITORY
+            </a>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else if (this.props.selectedModal == 'clone-repo') {
+      return (
+        <div id="modal1" className={`modal + ${this.props.modalDisplayClass}`}>
+          <div className="modal-content white">
+            <h4>CLONE REPOSITORY</h4>
+            <h6>Add Url</h6>
+            <ul className="collection">
+              <form>
+                <input
+                  onChange={this.onChange}
+                  placeholder="Enter the Url to Clone..."
+                  id="repo-url"
+                  type="text"
+                  class="validate"
+                />
+              </form>
+            </ul>
+          </div>
+          <div className="modal-footer">
+            <a
+              className="modal-close waves-effect waves-green btn-flat"
+              onClick={() => {
+                this.props.toggleOverlay();
+                this.props.toggleModalClass();
+              }}
+            >
+              CLOSE
+            </a>
+            <a
+              className="add-repo waves-effect waves-green btn-flat blue darken-2 white-text"
+              onClick={() => {
+                this.props.toggleOverlay();
+                this.props.toggleModalClass();
+                this.props.setRepoDetailsDisplayClass();
+                // shell.openItem(this.state.path[0]);
+                // Opens the chosen folder/file in a new window
+              }}
+            >
+              CLONE REPOSITORY
+            </a>
+          </div>
+        </div>
+      );
+    }
   }
-  else if (this.props.selectedModal == 'clone-repo'){
-    return (
-      <div id="modal1" className={`modal + ${this.props.modalDisplayClass}`}>
-        <div className="modal-content white">
-          <h4>CLONE REPOSITORY</h4>
-          <h6>Add Url</h6>
-          <ul className="collection">
-          <form >
-          <input onChange = {this.onChange} placeholder="Enter the Url to Clone..." id="repo-url" type="text" class="validate" />
-        </form>
-          </ul>
-        </div>
-        <div className="modal-footer">
-          <a
-            className="modal-close waves-effect waves-green btn-flat"
-            onClick={() => {
-              this.props.toggleOverlay();
-              this.props.toggleModalClass();
-            }}
-          >
-            CLOSE
-          </a>
-          <a
-            className="add-repo waves-effect waves-green btn-flat blue darken-2 white-text"
-            onClick={() => {
-              this.props.toggleOverlay();
-              this.props.toggleModalClass();
-              this.props.setRepoDetailsDisplayClass();
-              // shell.openItem(this.state.path[0]);
-               // Opens the chosen folder/file in a new window
-            }}
-          >
-            CLONE REPOSITORY
-          </a>
-        </div>
-      </div>
-    );
-  }
-}
 }
--- src/components/showCommitDetails.jsx
+++ src/components/showCommitDetails.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 
@@ -6,23 +9,25 @@ class showCommitDetails extends Component {
     const { commitHistory } = this.props.commitHistory;
     // console.log(commitHistory);
     return (
-      <React.Fragment>
+      <>
         <div>
-        <h4>Commit Details:</h4>
-            {commitHistory.map((commit) => 
-                  <div className='commit-message'>
-                    {/* <p>SHA: {commit.hash}</p> */}
-                    <button onClick={this.props.getSelectedCommit.bind(this, commit.hash)}>SHA: {commit.hash}</button>
-                    <p>Message: {commit.message}</p>
-                    <p>Author: {commit.author_name}</p>
-                    <p>Author Email: {commit.author_email}</p>
-                    <p>Timestamp: {commit.date}</p>
-                    <p>-----------------------------------------------------------------------------</p>
-                  </div>
-                )}
+          <h4>Commit Details:</h4>
+          {commitHistory.map(commit => (
+            <div className="commit-message">
+              {/* <p>SHA: {commit.hash}</p> */}
+              <button onClick={this.props.getSelectedCommit.bind(this, commit.hash)}>
+                SHA: {commit.hash}
+              </button>
+              <p>Message: {commit.message}</p>
+              <p>Author: {commit.author_name}</p>
+              <p>Author Email: {commit.author_email}</p>
+              <p>Timestamp: {commit.date}</p>
+              <p>-----------------------------------------------------------------------------</p>
             </div>
-          </React.Fragment>
-          );
+          ))}
+        </div>
+      </>
+    );
   }
 }
 
