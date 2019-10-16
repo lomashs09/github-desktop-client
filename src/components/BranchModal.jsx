c289efa764c4fc409fc437304c32299f8f24bdf7
Lomash Sharma <lomash.sharma@mountblue.io>
2019-10-16 12:48:02 +0530

    handled reference error for createNewBranch

--- src/components/BranchModal.jsx
+++ src/components/BranchModal.jsx
@@ -35,7 +35,7 @@ export class BranchModal extends Component {
     const git = require('simple-git')(filePath);
     git.checkoutBranch(this.state.newBranch, 'master', (err, results) => {
       if (err) {
-        console.log(err);
+        console.log('The following error Ocurred:',err);
       } else {
         this.setState({ successMessage: 'Branch created successfully' });
         var joined = this.state.branches.concat(this.state.newBranch);
