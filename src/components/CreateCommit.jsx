22823504bc4c113fc31285dd9e4d3c4bf5d451d5
amoghwagh <amoghwagh@gmail.com>
2019-10-18 20:26:57 +0530

    Fixed css in commit-history layout

--- src/components/CreateCommit.jsx
+++ src/components/CreateCommit.jsx
@@ -4,29 +4,37 @@ import React, { Component } from 'react';
 export class CreateCommit extends Component {
   state = {
     commitMessage: ''
-  }
+  };
 
-  setCommitMessage = (event) => {
+  setCommitMessage = event => {
     this.setState({
       commitMessage: event.target.value
     });
-  }
-  
+  };
+
   render() {
     return (
-        <>
-        <div class="row">
+      <>
         <form class="col s12">
           <div class="row">
             <div class="input-field col s12">
-              <textarea id="textarea1" class="materialize-textarea" onChange={this.setCommitMessage}></textarea>
+              <textarea
+                id="textarea1"
+                class="materialize-textarea"
+                onChange={this.setCommitMessage}
+              ></textarea>
               <label for="textarea1">Commit Message (Required)</label>
             </div>
           </div>
         </form>
-      </div>
-      <a class="waves-effect waves-light btn blue darken-2 commit-button" onClick={this.props.makeCommit.bind(this, this.state.commitMessage)}>Commit</a>
-    </>
+
+        <a
+          class="waves-effect waves-light btn blue darken-2 commit-button"
+          onClick={this.props.makeCommit.bind(this, this.state.commitMessage)}
+        >
+          Commit
+        </a>
+      </>
     );
   }
 }
